1cfb0c8df6e020b964744228268940eecca34170
JorgeIgnacioCondoriAguirre <jcondoriaguirre@frba.utn.edu.ar>
2026-05-19 16:24:57 -0300

    arreglado

--- src/main/java/ar/edu/utn/dds/k3003/repositories/InMemoryDonadoresRepo.java
+++ src/main/java/ar/edu/utn/dds/k3003/repositories/InMemoryDonadoresRepo.java
@@ -1,47 +1,52 @@
 package ar.edu.utn.dds.k3003.repositories;
 
 import ar.edu.utn.dds.k3003.model.Donador;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
+import lombok.val;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class InMemoryDonadoresRepo implements DonadoresRepository {
-  EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("db");
-  EntityManager entityManager = entityManagerFactory .createEntityManager();
+
+  private List<Donador> donadores;
+  private AtomicLong idSecuencial = new AtomicLong(1);
+
+  public InMemoryDonadoresRepo() {
+    this.donadores = new ArrayList<>();
+  }
+
 
   @Override
   public Optional<Donador> findById(String id) {
-    Donador donador = entityManager.find(Donador.class, id);
-    return Optional.ofNullable(donador);
+
+    return this.donadores.stream()
+            .filter(d -> Objects.equals(d.getId(), id))
+            .findFirst();
   }
 
   @Override
   public Donador save(Donador donador) {
-    if (donador.getId() == null || this.findById(donador.getId()).isEmpty()) {
-      entityManager.persist(donador);
-      return donador;
-    } else {
-      return entityManager.merge(donador);
+    if (donador.getId() == null || donador.getId().isEmpty()) {
+      donador.setId(String.valueOf(idSecuencial.getAndIncrement()));
     }
+    this.donadores.add(donador);
+    return donador;
   }
 
   @Override
   public Donador deleteById(String id) {
-    var donadorOptional = this.findById(id);
-    if (donadorOptional.isPresent()) {
-      Donador donador = donadorOptional.get();
-      entityManager.remove(donador);
-      return donador;
-    }
-    return null;
+    val donador = this.findById(id);
+    this.donadores.remove(donador.get());
+    return donador.get();
   }
 
+
   @Override
-  public List<Donador> todosLosDonadores() {
-    return entityManager.createQuery("SELECT Donador FROM Donador donador", Donador.class).getResultList();
+  public List<Donador> todosLosDonadores(){
+    return donadores.stream().toList();
   }
 }
 
