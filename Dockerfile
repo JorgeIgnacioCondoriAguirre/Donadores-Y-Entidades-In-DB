bf5e6c79ef6494c89f95015516ec7b64ceb4f4c3
JorgeIgnacioCondoriAguirre <jcondoriaguirre@frba.utn.edu.ar>
2026-05-25 21:45:26 -0300

    Entidades relacionadas

--- Dockerfile
+++ Dockerfile
@@ -10,4 +10,4 @@ FROM eclipse-temurin:21-jre-jammy
 WORKDIR /app
 COPY --from=build /app/target/*.jar app.jar
 EXPOSE 8080
-CMD ["java","-jar","app.jar"]
+CMD ["java", "-Xmx450m", "-jar", "app.jar"]
--- src/main/java/ar/edu/utn/dds/k3003/Application.java
+++ src/main/java/ar/edu/utn/dds/k3003/Application.java
@@ -1,15 +1,19 @@
 package ar.edu.utn.dds.k3003;
 
-import ar.edu.utn.dds.k3003.catedra.dtos.donadoresYEntidades.DonadorDTO;
-import ar.edu.utn.dds.k3003.catedra.dtos.donadoresYEntidades.EstadoDonadorEnum;
 import ar.edu.utn.dds.k3003.repositories.*;
 import jakarta.persistence.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+})
 public class Application {
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
